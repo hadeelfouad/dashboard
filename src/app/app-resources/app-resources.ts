6dd8d8f789084fe322bc7fb8f04cffb0e2e1e1e8
Hossam Ahmed <bahaah@emc.com>
2018-06-24 11:01:27 +0100

    Adding clarity design and enhance the ui

--- src/app/app-resources/app-resources.ts
+++ src/app/app-resources/app-resources.ts
@@ -3,32 +3,44 @@ import { AppResource } from "src/app/app-resources/app-resource"
 export const APP_RESOURCES: AppResource[] = [
     { 
         "name" : "trivia competiton",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "Test your knowledge"
     
     },
     { 
         "name" : "fish game",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "A new fish game !!"
     
     },
     { 
         "name" : "skills portal",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "skills portal"
     
     },
     { 
         "name" : "pictionary",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "pictionary"
     
     },
     { 
         "name" : "suggestion box",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "suggestion box"
     
     },
     { 
         "name" : "GIF generator",
-        "url": "http://www.google.com"
+        "url": "http://www.google.com",
+        "pic": "",
+        "description": "GIF generator"
     
     }
 ]
